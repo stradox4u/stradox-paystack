19c3b93e7a82480de373c09f268b610035cda307
Odeh Adejoh <stradox@gmail.com>
2024-11-22 01:39:38 +0100

    add module and example doc in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
+/**
+ * This module grants access to the Paystack object, with which we can access the various resources in the Paystack API
+ * @module
+ * 
+ * @example
+ * ```ts
+ * import * as Paystack from "@stradox/paystack";
+ * 
+ * const paystack = Paystack(<your_secret_key>);
+ * 
+ * const transaction = await paystack.transaction.initialize({
+ *  amount: 10000,
+ *  email: "johndoe@test.com",
+ * });
+ *
+ */
+
 import PaystackApi from "./paystackApi.ts";
 
 let paystackInstance: PaystackApi;
