2e3806ad1fe3393681663649c6dfe53d5b44e15f
Odeh Adejoh <stradox@gmail.com>
2024-11-24 17:20:30 +0100

    implemented charge resource

---
+++ src/resources/charge.ts
@@ -0,0 +1,102 @@
+import type { CreateChargeBody, SubmitAddressBody, SubmitBirthdayBody, SubmitOtpBody, SubmitPhoneBody, SubmitPinBody } from "../types/charge.ts";
+import type { PaystackResponseInterface } from "../types/response.ts";
+import PaystackShared from "./paystackShared.ts";
+
+export default class Charge extends PaystackShared {
+  private readonly resourceUrl = '/charge';
+
+  constructor(secretKey: string) {
+    super(secretKey);
+  }
+
+  /**
+   * @function create
+   * Initiate a payment by integrating the payment channel of your choice.
+   * @param body
+   * @returns {Promise<PaystackResponseInterface | null>}
+   */
+  public create = async (body: CreateChargeBody): Promise<PaystackResponseInterface | null> => {
+    const url = this.resourceUrl;
+    const method = 'POST';
+
+    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
+  }
+
+  /**
+   * @function submitPin
+   * Submit PIN to continue a charge
+   * @param body
+   * @returns {Promise<PaystackResponseInterface | null>}
+   */
+  public submitPin = async (body: SubmitPinBody): Promise<PaystackResponseInterface | null> => {
+    const url = this.resourceUrl + '/submit_pin';
+    const method = 'POST';
+
+    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
+  }
+
+  /**
+   * @function submitOtp
+   * Submit OTP to complete a charge
+   * @param body
+   * @returns {Promise<PaystackResponseInterface | null>}
+   */
+  public submitOtp = async (body: SubmitOtpBody): Promise<PaystackResponseInterface | null> => {
+    const url = this.resourceUrl + '/submit_otp';
+    const method = 'POST';
+
+    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
+  }
+
+  /**
+   * @function submitPhone
+   * Submit phone number when requested
+   * @param body
+   * @returns {Promise<PaystackResponseInterface | null>}
+   */
+  public submitPhone = async (body: SubmitPhoneBody): Promise<PaystackResponseInterface | null> => {
+    const url = this.resourceUrl + '/submit_phone';
+    const method = 'POST';
+
+    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
+  }
+
+  /**
+   * @function submitBirthday
+   * Submit birthday when requested
+   * @param body
+   * @returns {Promise<PaystackResponseInterface | null>}
+   */
+  public submitBirthday = async (body: SubmitBirthdayBody): Promise<PaystackResponseInterface | null> => {
+    const url = this.resourceUrl + '/submit_birthday';
+    const method = 'POST';
+
+    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
+  }
+
+  /**
+   * @function submitAddress
+   * Submit address to continue a charge
+   * @param body
+   * @returns {Promise<PaystackResponseInterface | null>}
+   */
+  public submitAddress = async (body: SubmitAddressBody): Promise<PaystackResponseInterface | null> => {
+    const url = this.resourceUrl + '/submit_address';
+    const method = 'POST';
+
+    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
+  }
+
+  /**
+   * @function checkPendingCharge
+   * Check pending charge
+   * @param reference - The reference to check
+   * @returns {Promise<PaystackResponseInterface | null>}
+   */
+  public checkPendingCharge = async (reference: string): Promise<PaystackResponseInterface | null> => {
+    const url = this.resourceUrl + '/:reference';
+    const method = 'GET';
+
+    return await this.paystackFetch(url, method, {}, { reference });
+  }
+}
---
+++ src/types/charge.ts
@@ -0,0 +1,80 @@
+export interface CreateChargeBody {
+  /** Customer's email address */
+  email: string;
+  /** Amount in subunit of the supported currency */
+  amount: number;
+  /** The split code of a previously created split. e.g. `SPL_98WF13Eb3w` */
+  split_code?: string;
+  /** The code for the subaccount that owns the payment. e.g. `ACCT_8f4s1eq7ml6rlzj` */
+  subaccount?: string;
+  /** An amount used to override the split configuration for a single split payment. If set, the amount
+   * specified goes to the main account regardless of the split configuration.
+   */
+  transaction_charge?: number;
+  /** Use this param to indicate who bears the transaction charges. Allowed values are: `account` or `subaccount`
+   * (defaults to `account`)
+   */
+  bearer?: 'account' | 'subaccount';
+  /** Bank account to charge (don't send if charging an authorization code) */
+  bank?: {
+    code: string;
+    account_number: string;
+  }
+  /** Takes the settings for the Pay with Transfer (PwT) channel. Pass in the `account_expires_at` param
+   * to set the expiry time.
+   */
+  bank_transfer?: Record<string, unknown>;
+  /** USSD type to charge (don't send if charging an authorization code, bank or card) */
+  ussd?: Record<string, unknown>;
+  /** Mobile details (don't send if charging an authorization code, bank or card) */
+  mobile_money?: Record<string, unknown>;
+  /** Takes a provider object with either of the following values: `scan-to-pay`, `visa` */
+  qr?: Record<string, unknown>;
+  /** An authorization code to charge (don't send if charging a bank account) */
+  authorization_code?: string;
+  /** 4-digit PIN (send with a non-reusable authorization code) */
+  pin?: string;
+  /** Used for passing additional details for your post-payment processes */
+  metadata?: Record<string, unknown>;
+  /** Unique transaction reference. Only `-`, `.`, `=` and alphanumeric characters allowed */
+  reference?: string;
+  /** This is the unique identifier of the device a user uses in making payment. Only `-`, `.`,
+   * `=` and alphanumeric characters allowed */
+  device_id?: string;
+}
+
+interface ReferenceOnly {
+  /** Reference for ongoing transaction */
+  reference: string;
+}
+
+export interface SubmitPinBody extends ReferenceOnly {
+  /** PIN submitted by user */
+  pin: string;
+}
+
+export interface SubmitOtpBody extends ReferenceOnly {
+  /** OTP submitted by user */
+  otp: string;
+}
+
+export interface SubmitPhoneBody extends ReferenceOnly {
+  /** Phone submitted by user */
+  phone: string;
+}
+
+export interface SubmitBirthdayBody extends ReferenceOnly {
+  /** Birthday submitted by user in YYYY-MM-DD format */
+  birthday: string;
+}
+
+export interface SubmitAddressBody extends ReferenceOnly {
+  /** Address submitted by user */
+  address: string;
+  /** City submitted by user */
+  city: string;
+  /** State submitted by user */
+  state: string;
+  /** Zipcode submitted by user */
+  zipcode: string;
+}
