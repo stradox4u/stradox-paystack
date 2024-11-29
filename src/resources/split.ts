fc7e3731c1890aceb4af28ead7a788a5e186fa48
Odeh Adejoh <stradox@gmail.com>
2024-11-29 08:39:40 +0100

    entered return types for subaccount split resource

--- src/resources/split.ts
+++ src/resources/split.ts
@@ -1,12 +1,23 @@
 import type { PaystackResponseInterface } from "../types/response.ts";
-import type { AddSubaccountSplitBody, CreateSplitBody, ListSplitQueries, UpdateSplitBody } from "../types/split.ts";
+import type {
+  AddSubaccountSplitBody,
+  AddSubaccountSplitData,
+  CreateSplitBody,
+  CreateSplitData,
+  FetchSplitData,
+  ListSplitQueries,
+  ListSplitsDatum,
+  ListSplitsMeta,
+  UpdateSplitBody,
+  UpdateSplitData,
+} from "../types/split.ts";
 import PaystackShared from "./paystackShared.ts";
 
 /**
  * This class contains methods for working with the Split resource of the Paystack API
  */
 export default class Split extends PaystackShared {
-  private readonly resourceUrl = '/split';
+  private readonly resourceUrl = "/split";
 
   constructor(secretKey: string) {
     super(secretKey);
@@ -15,75 +26,119 @@ export default class Split extends PaystackShared {
   /**
    * @function create
    * @param {CreateSplitBody} body
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<CreateSplitData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type CreateSplitData
    */
-  public create = async (body: CreateSplitBody): Promise<PaystackResponseInterface | null> => {
+  public create = async (
+    body: CreateSplitBody,
+  ): Promise<PaystackResponseInterface<CreateSplitData> | null> => {
     const url = this.resourceUrl;
-    const method = 'POST';
-  
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
-  }
+    const method = "POST";
+
+    return await this.paystackFetch<CreateSplitData>(
+      url,
+      method,
+      body as unknown as Record<string, unknown>,
+    );
+  };
 
   /**
    * @function list
    * @param {ListSplitQueries} queries
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<ListSplitsDatum[], ListSplitsMeta> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type ListSplitsDatum[]
    */
-  public list = async (queries: ListSplitQueries): Promise<PaystackResponseInterface | null> => {
+  public list = async (
+    queries: ListSplitQueries,
+  ): Promise<
+    PaystackResponseInterface<ListSplitsDatum[], ListSplitsMeta> | null
+  > => {
     const url = this.resourceUrl;
-    const method = 'GET';
+    const method = "GET";
 
-    return await this.paystackFetch(url, method, {}, {}, queries as unknown as Record<string, unknown>);
-  }
+    return await this.paystackFetch<ListSplitsDatum[], ListSplitsMeta>(
+      url,
+      method,
+      {},
+      {},
+      queries as unknown as Record<string, unknown>,
+    );
+  };
 
   /**
    * @function fetch
    * @param {string} splitId
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<FetchSplitData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type FetchSplitData
    */
-  public fetch = async (splitId: string): Promise<PaystackResponseInterface | null> => {
-    const url = this.resourceUrl + '/:splitId';
-    const method = 'GET';
-  
-    return await this.paystackFetch(url, method, {}, { splitId });
-  }
+  public fetch = async (
+    splitId: string,
+  ): Promise<PaystackResponseInterface<FetchSplitData> | null> => {
+    const url = this.resourceUrl + "/:splitId";
+    const method = "GET";
+
+    return await this.paystackFetch<FetchSplitData>(url, method, {}, {
+      splitId,
+    });
+  };
 
   /**
    * @function update
    * @param {string} splitId
    * @param {UpdateSplitBody} body
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<UpdateSplitData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type UpdateSplitData
    */
-  public update = async (splitId: string, body: UpdateSplitBody): Promise<PaystackResponseInterface | null> => {
-    const url = this.resourceUrl + '/:splitId';
-    const method = 'PUT';
-  
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>, { splitId });
-  }
+  public update = async (
+    splitId: string,
+    body: UpdateSplitBody,
+  ): Promise<PaystackResponseInterface<UpdateSplitData> | null> => {
+    const url = this.resourceUrl + "/:splitId";
+    const method = "PUT";
+
+    return await this.paystackFetch<UpdateSplitData>(
+      url,
+      method,
+      body as unknown as Record<string, unknown>,
+      { splitId },
+    );
+  };
 
   /**
    * @function addSubaccountSplit
    * @param {string} splitId
    * @param {AddSubaccountSplitBody} body
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<AddSubaccountSplitData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type AddSubaccountSplitData
    */
-  public addSubaccountSplit = async (splitId: string, body: AddSubaccountSplitBody): Promise<PaystackResponseInterface | null> => {
-    const url = this.resourceUrl + '/:splitId/subaccount/add';
-    const method = 'POST';
-  
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>, { splitId });
-  }
+  public addSubaccountSplit = async (
+    splitId: string,
+    body: AddSubaccountSplitBody,
+  ): Promise<PaystackResponseInterface<AddSubaccountSplitData> | null> => {
+    const url = this.resourceUrl + "/:splitId/subaccount/add";
+    const method = "POST";
+
+    return await this.paystackFetch<AddSubaccountSplitData>(
+      url,
+      method,
+      body as unknown as Record<string, unknown>,
+      { splitId },
+    );
+  };
 
   /**
    * @function removeSubaccountSplit
    * @param {string} splitId
    * @param {Record<string, string>} body
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<undefined> | null>} response - A promise that resolves to the PaystackResponseInterface type, with a boolean status property and a message property
    */
-  public removeSubaccountSplit = async(splitId: string, body: { subaccount: string }): Promise<PaystackResponseInterface | null> => {
-    const url = this.resourceUrl + '/:splitId/subaccount/remove';
-    const method = 'POST';
-  
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>, { splitId });
-  }
+  public removeSubaccountSplit = async (
+    splitId: string,
+    body: { subaccount: string },
+  ): Promise<PaystackResponseInterface<undefined> | null> => {
+    const url = this.resourceUrl + "/:splitId/subaccount/remove";
+    const method = "POST";
+
+    return await this.paystackFetch<undefined>(
+      url,
+      method,
+      body as unknown as Record<string, unknown>,
+      { splitId },
+    );
+  };
 }
--- src/types/split.ts
+++ src/types/split.ts
@@ -31,3 +31,58 @@ export interface AddSubaccountSplitBody {
   subaccount: string;
   share: number;
 }
+
+export interface CreateSplitData {
+  id: number;
+  name: string;
+  type: string;
+  currency: string;
+  integration: number;
+  domain: string;
+  split_code: string;
+  active: boolean;
+  bearer_type: string;
+  createdAt: string;
+  updatedAt: string;
+  is_dynamic: boolean;
+  subaccounts: Subaccount[];
+  total_subaccounts: number;
+}
+
+interface Subaccount {
+  subaccount: SubaccountDetail;
+  share: number;
+}
+
+interface SubaccountDetail {
+  id: number;
+  subaccount_code: string;
+  business_name: string;
+  description: string;
+  primary_contact_name: string;
+  primary_contact_email: string;
+  primary_contact_phone: string;
+  metadata: Record<string, unknown>;
+  settlement_bank: string;
+  currency: string;
+  account_number: string;
+}
+
+export interface ListSplitsMeta {
+  total: number;
+  skipped: number;
+  perPage: number;
+  page: number;
+  pageCount: number;
+}
+
+export interface ListSplitsDatum extends CreateSplitData {
+  bearer_subaccount: string;
+}
+
+export interface FetchSplitData extends ListSplitsDatum {}
+
+export interface UpdateSplitData extends ListSplitsDatum {}
+
+export interface AddSubaccountSplitData extends ListSplitsDatum{}
+
