3d5c515428c2fd003db6bf3e7ca2ce019da69e2d
Odeh Adejoh <stradox@gmail.com>
2024-11-29 09:08:35 +0100

    inserted return types for subaccount and subsciption resources

--- src/resources/subaccount.ts
+++ src/resources/subaccount.ts
@@ -1,5 +1,5 @@
 import type { PaystackResponseInterface } from "../types/response.ts";
-import type { CreateSubaccountBody, ListSubaccountQueries, UpdateSubaccountBody } from "../types/subaccount.ts";
+import type { CreateSubaccountBody, CreateSubaccountData, FetchSubaccountData, ListSubaccountQueries, ListSubaccountsDatum, ListSubaccountsMeta, UpdateSubaccountBody, UpdateSubaccountData } from "../types/subaccount.ts";
 import PaystackShared from "./paystackShared.ts";
 
 export default class Subaccount extends PaystackShared {
@@ -13,39 +13,39 @@ export default class Subaccount extends PaystackShared {
    * @function create
    * Create a subaccount on your integration
    * @param body 
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<CreateSubaccountData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type CreateSubaccountData
    */
-  public create = async (body: CreateSubaccountBody): Promise<PaystackResponseInterface | null> => {
+  public create = async (body: CreateSubaccountBody): Promise<PaystackResponseInterface<CreateSubaccountData> | null> => {
     const url = this.resourceUrl;
     const method = 'POST';
 
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
+    return await this.paystackFetch<CreateSubaccountData>(url, method, body as unknown as Record<string, unknown>);
   }
 
   /**
    * @function list
    * List subaccounts available on your integration
    * @param queries
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<ListSubaccountsDatum[], ListSubaccountsMeta> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type ListSubaccountsDatum[]
    */
-  public list = async (queries: ListSubaccountQueries): Promise<PaystackResponseInterface | null> => {
+  public list = async (queries: ListSubaccountQueries): Promise<PaystackResponseInterface<ListSubaccountsDatum[], ListSubaccountsMeta> | null> => {
     const url = this.resourceUrl;
     const method = 'GET';
 
-    return await this.paystackFetch(url, method, {}, {}, queries as unknown as Record<string, unknown>);
+    return await this.paystackFetch<ListSubaccountsDatum[], ListSubaccountsMeta>(url, method, {}, {}, queries as unknown as Record<string, unknown>);
   }
 
   /**
    * @function fetch
    * Get details of a subaccount on your integration
    * @param subaccountCode - The subaccount id or code you want to fetch
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<FetchSubaccountData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type FetchSubaccountData
    */
-  public fetch = async (subaccountCode: string): Promise<PaystackResponseInterface | null> => {
+  public fetch = async (subaccountCode: string): Promise<PaystackResponseInterface<FetchSubaccountData> | null> => {
     const url = this.resourceUrl + '/:subaccountCode';
     const method = 'GET';
 
-    return await this.paystackFetch(url, method, {}, { subaccountCode });
+    return await this.paystackFetch<FetchSubaccountData>(url, method, {}, { subaccountCode });
   }
 
   /**
@@ -53,12 +53,12 @@ export default class Subaccount extends PaystackShared {
    * Update a subaccount's details on your integration
    * @param subaccountCode - The subaccount id or code you want to update
    * @param body 
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<UpdateSubaccountData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type UpdateSubaccountData
    */
-  public update = async (subaccountCode: string, body: UpdateSubaccountBody): Promise<PaystackResponseInterface | null> => {
+  public update = async (subaccountCode: string, body: UpdateSubaccountBody): Promise<PaystackResponseInterface<UpdateSubaccountData> | null> => {
     const url = this.resourceUrl + '/:subaccountCode';
     const method = 'PUT';
 
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>, { subaccountCode });
+    return await this.paystackFetch<UpdateSubaccountData>(url, method, body as unknown as Record<string, unknown>, { subaccountCode });
   }
 }
--- src/resources/subscription.ts
+++ src/resources/subscription.ts
@@ -1,5 +1,5 @@
 import type { PaystackResponseInterface } from "../types/response.ts";
-import type { CreateSubscriptionBody, ListSubscriptionQueries, ToggleSubscriptionBody } from "../types/subscription.ts";
+import type { CreateSubscriptionBody, FetchSubscriptionData, GenerateLinkData, ListSubscriptionQueries, ListSubscriptionsDatum, ListSubscriptionsMeta, ToggleSubscriptionBody } from "../types/subscription.ts";
 import PaystackShared from "./paystackShared.ts";
 
 export default class Subscription extends PaystackShared {
@@ -13,90 +13,90 @@ export default class Subscription extends PaystackShared {
    * @function create
    * Create a subsciption on your integration
    * @param body
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<CreateSubscriptionBody> | null>} response - A promise that resolves to an object with a data property of type CreateSubscriptionBody
    */
-  public create = async (body: CreateSubscriptionBody): Promise<PaystackResponseInterface | null> => {
+  public create = async (body: CreateSubscriptionBody): Promise<PaystackResponseInterface<CreateSubscriptionBody> | null> => {
     const url = this.resourceUrl;
     const method = 'POST';
 
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
+    return await this.paystackFetch<CreateSubscriptionBody>(url, method, body as unknown as Record<string, unknown>);
   }
 
   /**
    * @function list
    * List subscriptions available on your integration
    * @param queries
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<ListSubscriptionsDatum[], ListSubscriptionsMeta> | null>} response - A promise that resolves to an object with data property of type ListSubscriptionsDatum[] and meta property of type ListSubscriptionsMeta
    */
-  public list = async (queries: ListSubscriptionQueries): Promise<PaystackResponseInterface | null> => {
+  public list = async (queries: ListSubscriptionQueries): Promise<PaystackResponseInterface<ListSubscriptionsDatum[], ListSubscriptionsMeta> | null> => {
     const url = this.resourceUrl;
     const method = 'GET';
 
-    return await this.paystackFetch(url, method, {}, {}, queries as unknown as Record<string, unknown>);
+    return await this.paystackFetch<ListSubscriptionsDatum[], ListSubscriptionsMeta>(url, method, {}, {}, queries as unknown as Record<string, unknown>);
   }
 
   /**
    * @function fetch
    * Get details of a subscription on your integration
    * @param idOrCode - The subscription `ID` or `code` you want to fetch
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<FetchSubscriptionData> | null>} response - A promise that resolves to an object with a data property of type FetchSubscriptionData
    */
-  public fetch = async (idOrCode: string): Promise<PaystackResponseInterface | null> => {
+  public fetch = async (idOrCode: string): Promise<PaystackResponseInterface<FetchSubscriptionData> | null> => {
     const url = this.resourceUrl + '/:idOrCode';
     const method = 'GET';
 
-    return await this.paystackFetch(url, method, {}, { idOrCode });
+    return await this.paystackFetch<FetchSubscriptionData>(url, method, {}, { idOrCode });
   }
 
   /**
    * @function enable
    * Enable a subscription on your integration
    * @param body
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<undefined> | null>} response - A promise that resolves to an object with a boolean status property and a message property
    */
-  public enable = async (body: ToggleSubscriptionBody): Promise<PaystackResponseInterface | null> => {
+  public enable = async (body: ToggleSubscriptionBody): Promise<PaystackResponseInterface<undefined> | null> => {
     const url = this.resourceUrl + '/enable';
     const method = 'POST';
 
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
+    return await this.paystackFetch<undefined>(url, method, body as unknown as Record<string, unknown>);
   }
 
   /**
    * @function disable
    * Disable a subscription on your integration
    * @param body
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<undefined> | null>} response - A promise that resolves to an object with a boolean status property and a message property
    */
-  public disable = async (body: ToggleSubscriptionBody): Promise<PaystackResponseInterface | null> => {
+  public disable = async (body: ToggleSubscriptionBody): Promise<PaystackResponseInterface<undefined> | null> => {
     const url = this.resourceUrl + '/disable';
     const method = 'POST';
 
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
+    return await this.paystackFetch<undefined>(url, method, body as unknown as Record<string, unknown>);
   }
 
   /**
    * @function generateLink
    * Generate a link for updating the card on a subscription
    * @param code - The subscription code
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<GenerateLinkData> | null>} response - A promise that resolves to an object with a data property of type GenerateLinkData
    */
-  public generateLink = async (code: string): Promise<PaystackResponseInterface | null> => {
+  public generateLink = async (code: string): Promise<PaystackResponseInterface<GenerateLinkData> | null> => {
     const url = this.resourceUrl + '/:code/manage/link';
     const method = 'GET';
 
-    return await this.paystackFetch(url, method, {}, { code });
+    return await this.paystackFetch<GenerateLinkData>(url, method, {}, { code });
   }
 
   /**
    * @function sendLink
    * Email a customer a link for updating the card on their subscription
    * @param code - The subscription code
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<undefined> | null>} response - A promise that resolves to an object with a boolean status property and a message property
    */
-  public sendLink = async (code: string): Promise<PaystackResponseInterface | null> => {
+  public sendLink = async (code: string): Promise<PaystackResponseInterface<undefined> | null> => {
     const url = this.resourceUrl + '/:code/manage/email';
     const method = 'POST';
 
-    return await this.paystackFetch(url, method, {}, { code });
+    return await this.paystackFetch<undefined>(url, method, {}, { code });
   }
 }
