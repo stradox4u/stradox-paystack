674e8d13a1a903892f0a2996a1349da6afd7e6c2
Odeh Adejoh <stradox@gmail.com>
2024-11-29 10:31:45 +0100

    formatted virtualAccount.ts resource file

--- src/resources/virtualAccount.ts
+++ src/resources/virtualAccount.ts
@@ -1,9 +1,23 @@
 import type { PaystackResponseInterface } from "../types/response.ts";
-import type { AssignVirtualAccountBody, CreateVirtualAccountBody, CreateVirtualAccountData, DeactivateVirtualAccountData, FetchProvidersDatum, FetchVirtualAccountData, ListVirtualAccountQueries, ListVirtualAccountsDatum, ListVirtualAccountsMeta, RemoveSplitData, RequeryVirtualAccountQueries, SplitTransactionData, SplitVirtualAccountTransactionBody } from "../types/virtualAccounts.ts";
+import type {
+  AssignVirtualAccountBody,
+  CreateVirtualAccountBody,
+  CreateVirtualAccountData,
+  DeactivateVirtualAccountData,
+  FetchProvidersDatum,
+  FetchVirtualAccountData,
+  ListVirtualAccountQueries,
+  ListVirtualAccountsDatum,
+  ListVirtualAccountsMeta,
+  RemoveSplitData,
+  RequeryVirtualAccountQueries,
+  SplitTransactionData,
+  SplitVirtualAccountTransactionBody,
+} from "../types/virtualAccounts.ts";
 import PaystackShared from "./paystackShared.ts";
 
 export default class DedicatedVirtualAccount extends PaystackShared {
-  private readonly resourceUrl = '/dedicated_account';
+  private readonly resourceUrl = "/dedicated_account";
 
   constructor(secretKey: string) {
     super(secretKey);
@@ -12,15 +26,21 @@ export default class DedicatedVirtualAccount extends PaystackShared {
   /**
    * @function create
    * Create a dedicated virtual account for an existing customer
-   * @param body 
+   * @param body
    * @returns {Promise<PaystackResponseInterface<CreateVirtualAccountData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with a data property of type CreateVirtualAccountData
    */
-  public create = async (body: CreateVirtualAccountBody): Promise<PaystackResponseInterface<CreateVirtualAccountData> | null> => {
+  public create = async (
+    body: CreateVirtualAccountBody,
+  ): Promise<PaystackResponseInterface<CreateVirtualAccountData> | null> => {
     const url = this.resourceUrl;
-    const method = 'POST';
+    const method = "POST";
 
-    return await this.paystackFetch<CreateVirtualAccountData>(url, method, body as unknown as Record<string, unknown>);
-  }
+    return await this.paystackFetch<CreateVirtualAccountData>(
+      url,
+      method,
+      body as unknown as Record<string, unknown>,
+    );
+  };
 
   /**
    * @function assign
@@ -28,12 +48,18 @@ export default class DedicatedVirtualAccount extends PaystackShared {
    * @param body
    * @returns {Promise<PaystackResponseInterface<undefined> | null>} response - A promise that resolves to the PaystackResponseInterface type, with a boolean status property and a message property
    */
-  public assign = async (body: AssignVirtualAccountBody): Promise<PaystackResponseInterface<undefined> | null> => {
-    const url = this.resourceUrl + '/assign';
-    const method = 'POST';
+  public assign = async (
+    body: AssignVirtualAccountBody,
+  ): Promise<PaystackResponseInterface<undefined> | null> => {
+    const url = this.resourceUrl + "/assign";
+    const method = "POST";
 
-    return await this.paystackFetch<undefined>(url, method, body as unknown as Record<string, unknown>);
-  }
+    return await this.paystackFetch<undefined>(
+      url,
+      method,
+      body as unknown as Record<string, unknown>,
+    );
+  };
 
   /**
    * @function list
@@ -41,12 +67,22 @@ export default class DedicatedVirtualAccount extends PaystackShared {
    * @param queries
    * @returns {Promise<PaystackResponseInterface<ListVirtualAccountsDatum[], ListVirtualAccountsMeta> | null>} response - A promise that resolves to the PaystackResponseInterface type, with a data property of type ListVirtualAccountsDatum[] and a meta property of type ListVirtualAccountsMeta
    */
-  public list = async (queries: ListVirtualAccountQueries): Promise<PaystackResponseInterface<ListVirtualAccountsDatum[], ListVirtualAccountsMeta> | null> => {
+  public list = async (
+    queries: ListVirtualAccountQueries,
+  ): Promise<
+    PaystackResponseInterface<
+      ListVirtualAccountsDatum[],
+      ListVirtualAccountsMeta
+    > | null
+  > => {
     const url = this.resourceUrl;
-    const method = 'GET';
+    const method = "GET";
 
-    return await this.paystackFetch<ListVirtualAccountsDatum[], ListVirtualAccountsMeta>(url, method, {}, {}, queries as unknown as Record<string, unknown>);
-  }
+    return await this.paystackFetch<
+      ListVirtualAccountsDatum[],
+      ListVirtualAccountsMeta
+    >(url, method, {}, {}, queries as unknown as Record<string, unknown>);
+  };
 
   /**
    * @function fetch
@@ -54,12 +90,16 @@ export default class DedicatedVirtualAccount extends PaystackShared {
    * @param accountId
    * @returns {Promise<PaystackResponseInterface<FetchVirtualAccountData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with a data property of type FetchVirtualAccountData
    */
-  public fetch = async (accountId: string): Promise<PaystackResponseInterface<FetchVirtualAccountData> | null> => {
-    const url = this.resourceUrl + '/:accountId';
-    const method = 'GET';
+  public fetch = async (
+    accountId: string,
+  ): Promise<PaystackResponseInterface<FetchVirtualAccountData> | null> => {
+    const url = this.resourceUrl + "/:accountId";
+    const method = "GET";
 
-    return await this.paystackFetch<FetchVirtualAccountData>(url, method, {}, { accountId });
-  }
+    return await this.paystackFetch<FetchVirtualAccountData>(url, method, {}, {
+      accountId,
+    });
+  };
 
   /**
    * @function requery
@@ -67,12 +107,20 @@ export default class DedicatedVirtualAccount extends PaystackShared {
    * @param queries
    * @returns {Promise<PaystackResponseInterface<undefined> | null>} response - A promise that resolves to the PaystackResponseInterface type, with a boolean status property and a message property
    */
-  public requery = async (queries: RequeryVirtualAccountQueries): Promise<PaystackResponseInterface<undefined> | null> => {
-    const url = this.resourceUrl + '/requery';
-    const method = 'GET';
+  public requery = async (
+    queries: RequeryVirtualAccountQueries,
+  ): Promise<PaystackResponseInterface<undefined> | null> => {
+    const url = this.resourceUrl + "/requery";
+    const method = "GET";
 
-    return await this.paystackFetch<undefined>(url, method, {}, {}, queries as unknown as Record<string, unknown>);
-  }
+    return await this.paystackFetch<undefined>(
+      url,
+      method,
+      {},
+      {},
+      queries as unknown as Record<string, unknown>,
+    );
+  };
 
   /**
    * @function deactivate
@@ -80,12 +128,21 @@ export default class DedicatedVirtualAccount extends PaystackShared {
    * @param accountId
    * @returns {Promise<PaystackResponseInterface<DeactivateVirtualAccountData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with a data property of type DeactivateVirtualAccountData
    */
-  public deactivate = async (accountId: string): Promise<PaystackResponseInterface<DeactivateVirtualAccountData> | null> => {
-    const url = this.resourceUrl + '/:accountId';
-    const method = 'DELETE';
+  public deactivate = async (
+    accountId: string,
+  ): Promise<
+    PaystackResponseInterface<DeactivateVirtualAccountData> | null
+  > => {
+    const url = this.resourceUrl + "/:accountId";
+    const method = "DELETE";
 
-    return await this.paystackFetch<DeactivateVirtualAccountData>(url, method, {}, { accountId });
-  }
+    return await this.paystackFetch<DeactivateVirtualAccountData>(
+      url,
+      method,
+      {},
+      { accountId },
+    );
+  };
 
   /**
    * @function split
@@ -93,12 +150,18 @@ export default class DedicatedVirtualAccount extends PaystackShared {
    * @param body
    * @returns {Promise<PaystackResponseInterface<SplitTransactionData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with a data property of type SplitTransactionData
    */
-  public split = async (body: SplitVirtualAccountTransactionBody): Promise<PaystackResponseInterface<SplitTransactionData> | null> => {
-    const url = this.resourceUrl + '/split';
-    const method = 'POST';
+  public split = async (
+    body: SplitVirtualAccountTransactionBody,
+  ): Promise<PaystackResponseInterface<SplitTransactionData> | null> => {
+    const url = this.resourceUrl + "/split";
+    const method = "POST";
 
-    return await this.paystackFetch<SplitTransactionData>(url, method, body as unknown as Record<string, unknown>);
-  }
+    return await this.paystackFetch<SplitTransactionData>(
+      url,
+      method,
+      body as unknown as Record<string, unknown>,
+    );
+  };
 
   /**
    * @function removeSplit
@@ -107,22 +170,30 @@ export default class DedicatedVirtualAccount extends PaystackShared {
    * @param body - The dedicated virtual account number
    * @returns {Promise<PaystackResponseInterface<RemoveSplitData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with a data property of type RemoveSplitData
    */
-  public removeSplit = async (body: { account_number: string }): Promise<PaystackResponseInterface<RemoveSplitData> | null> => {
-    const url = this.resourceUrl + '/split';
-    const method = 'DELETE';
+  public removeSplit = async (
+    body: { account_number: string },
+  ): Promise<PaystackResponseInterface<RemoveSplitData> | null> => {
+    const url = this.resourceUrl + "/split";
+    const method = "DELETE";
 
-    return await this.paystackFetch<RemoveSplitData>(url, method, body as unknown as Record<string, unknown>);
-  }
+    return await this.paystackFetch<RemoveSplitData>(
+      url,
+      method,
+      body as unknown as Record<string, unknown>,
+    );
+  };
 
   /**
    * @function fetchProviders
    * Get available bank providers for a dedicated virtual account
    * @returns {Promise<PaystackResponseInterface<FetchProvidersDatum[]> | null>} response - A promise that resolves to the PaystackResponseInterface type, with a data property of type FetchProvidersDatum[]
    */
-  public fetchProviders = async (): Promise<PaystackResponseInterface<FetchProvidersDatum[]> | null> => {
-    const url = this.resourceUrl + '/available_providers';
-    const method = 'GET';
+  public fetchProviders = async (): Promise<
+    PaystackResponseInterface<FetchProvidersDatum[]> | null
+  > => {
+    const url = this.resourceUrl + "/available_providers";
+    const method = "GET";
 
     return await this.paystackFetch<FetchProvidersDatum[]>(url, method, {});
-  }
+  };
 }
