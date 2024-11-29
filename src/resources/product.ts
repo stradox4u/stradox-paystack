31cef33adf8e34e5e868a48cbd97d2f9eaead7d9
Odeh Adejoh <stradox@gmail.com>
2024-11-29 03:53:41 +0100

    inserted return types for product resource

--- src/resources/product.ts
+++ src/resources/product.ts
@@ -1,9 +1,17 @@
-import type { CreateProductBody, ListProductQueries } from "../types/product.ts";
+import type {
+  CreateProductBody,
+  CreateProductData,
+  FetchProductData,
+  ListProductQueries,
+  ListProductsDatum,
+  ListProductsMeta,
+  UpdateProductData,
+} from "../types/product.ts";
 import type { PaystackResponseInterface } from "../types/response.ts";
 import PaystackShared from "./paystackShared.ts";
 
 export default class Product extends PaystackShared {
-  private readonly resourceUrl = '/product';
+  private readonly resourceUrl = "/product";
 
   constructor(secretKey: string) {
     super(secretKey);
@@ -13,52 +21,78 @@ export default class Product extends PaystackShared {
    * @function create
    * Create a product on your integration
    * @param body
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<CreateProductData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type CreateProductData
    */
-  public create = async (body: CreateProductBody): Promise<PaystackResponseInterface | null> => {
+  public create = async (
+    body: CreateProductBody,
+  ): Promise<PaystackResponseInterface<CreateProductData> | null> => {
     const url = this.resourceUrl;
-    const method = 'POST';
+    const method = "POST";
 
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>);
-  }
+    return await this.paystackFetch<CreateProductData>(
+      url,
+      method,
+      body as unknown as Record<string, unknown>,
+    );
+  };
 
   /**
    * @function list
    * List products available on your integration
    * @param queries
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<ListProductsDatum[], ListProductsMeta> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type ListProductsDatum[]
    */
-  public list = async (queries: ListProductQueries): Promise<PaystackResponseInterface | null> => {
+  public list = async (
+    queries: ListProductQueries,
+  ): Promise<
+    PaystackResponseInterface<ListProductsDatum[], ListProductsMeta> | null
+  > => {
     const url = this.resourceUrl;
-    const method = 'GET';
+    const method = "GET";
 
-    return await this.paystackFetch(url, method, {}, {}, queries as unknown as Record<string, unknown>);
-  }
+    return await this.paystackFetch<ListProductsDatum[], ListProductsMeta>(
+      url,
+      method,
+      {},
+      {},
+      queries as unknown as Record<string, unknown>,
+    );
+  };
 
   /**
    * @function fetch
    * Get the details of a product on your integration
    * @param id - The product ID you want to fetch
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<FetchProductData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type FetchProductData
    */
-  public fetch = async (id: string): Promise<PaystackResponseInterface | null> => {
-    const url = this.resourceUrl + '/:id';
-    const method = 'GET';
+  public fetch = async (
+    id: string,
+  ): Promise<PaystackResponseInterface<FetchProductData> | null> => {
+    const url = this.resourceUrl + "/:id";
+    const method = "GET";
 
-    return await this.paystackFetch(url, method, {}, { id });
-  }
+    return await this.paystackFetch<FetchProductData>(url, method, {}, { id });
+  };
 
   /**
    * @function update
    * Update a product's details on your integration
    * @param id - The product ID you want to update
    * @param body
-   * @returns {Promise<PaystackResponseInterface | null>}
+   * @returns {Promise<PaystackResponseInterface<UpdateProductData> | null>} response - A promise that resolves to the PaystackResponseInterface type, with the data property being of type UpdateProductData
    */
-  public update = async (id: string, body: CreateProductBody): Promise<PaystackResponseInterface | null> => {
-    const url = this.resourceUrl + '/:id';
-    const method = 'PUT';
+  public update = async (
+    id: string,
+    body: CreateProductBody,
+  ): Promise<PaystackResponseInterface<UpdateProductData> | null> => {
+    const url = this.resourceUrl + "/:id";
+    const method = "PUT";
 
-    return await this.paystackFetch(url, method, body as unknown as Record<string, unknown>, { id });
-  }
+    return await this.paystackFetch<UpdateProductData>(
+      url,
+      method,
+      body as unknown as Record<string, unknown>,
+      { id },
+    );
+  };
 }
--- src/types/product.ts
+++ src/types/product.ts
@@ -17,3 +17,124 @@ export interface CreateProductBody {
 
 export interface ListProductQueries extends PaginatedList, DateRangedList {}
 
+export interface CreateProductData {
+  name: string;
+  description: string;
+  currency: string;
+  price: number;
+  quantity: number;
+  is_shippable: boolean;
+  unlimited: boolean;
+  integration: number;
+  domain: string;
+  metadata: Metadata;
+  slug: string;
+  product_code: string;
+  quantity_sold: number;
+  type: string;
+  shipping_fields: ShippingFields;
+  active: boolean;
+  in_stock: boolean;
+  minimum_orderable: number;
+  maximum_orderable: number;
+  low_stock_alert: boolean;
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface ShippingFields {
+  delivery_note: string;
+  shipping_fees?: ShippingFee[];
+}
+
+interface Metadata {
+  background_color: string;
+}
+
+export interface ListProductsMeta {
+  total: number;
+  skipped: number;
+  perPage: number;
+  page: number;
+  pageCount: number;
+}
+
+export interface ListProductsDatum {
+  id: number;
+  name: string;
+  description: string;
+  product_code: string;
+  slug: string;
+  currency: string;
+  price: number;
+  quantity: number;
+  quantity_sold: number;
+  active: boolean;
+  domain: string;
+  type: string;
+  in_stock: boolean;
+  unlimited: boolean;
+  metadata: Metadata;
+  files: File[];
+  success_message: null | string;
+  redirect_url: null | string;
+  split_code: null | string;
+  notification_emails: unknown[];
+  minimum_orderable: number;
+  maximum_orderable: number;
+  createdAt: string;
+  updatedAt: string;
+  digital_assets: unknown[];
+  variant_options: unknown[];
+  is_shippable: boolean;
+  shipping_fields: ShippingFields;
+  integration: number;
+  low_stock_alert: number;
+}
+
+interface ShippingFee {
+  region: string;
+  fee: number;
+  currency: string;
+}
+
+interface File {
+  key: string;
+  type: string;
+  path: string;
+  original_filename: string;
+}
+
+
+export interface FetchProductData extends ListProductsDatum {
+  file_path: string;
+  features: unknown[];
+  stock_threshold: number;
+  expires_in: number;
+}
+
+
+export interface UpdateProductData {
+  name: string;
+  description: string;
+  product_code: string;
+  price: number;
+  currency: string;
+  quantity: number;
+  quantity_sold: number;
+  type: string;
+  image_path: string;
+  file_path: string;
+  is_shippable: boolean;
+  unlimited: boolean;
+  domain: string;
+  active: boolean;
+  features: unknown[];
+  in_stock: boolean;
+  metadata: Record<string, unknown>;
+  id: number;
+  integration: number;
+  createdAt: string;
+  updatedAt: string;
+}
